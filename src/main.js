c1d55c144a7ed18753507215793a26101b3fe2f5
LEMONROOM <138027874+lemon-room@users.noreply.github.com>
2024-11-06 02:04:33 +0300

    firstUpdate

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-console.log('%cCreated By: Supadett Pinsuwannabut', 'background-color:red ;color: white; font-size: 30px; font-weight: bold');
-console.log('https://github.com/ToonKru3');
+console.log('%cCreated By: la6', 'background-color:green ;color: white; font-size: 21px; font-weight: bold');
+console.log('https://la6.su');
 import '/style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import { Pane } from 'tweakpane'
 
 
-let renderer , scene , camera , control;
-let box , material , mesh;
+let renderer, scene, camera, control;
+let box, material, mesh;
 
 const canvasWebGl = document.querySelector('canvas.webgl');
 
@@ -17,29 +16,14 @@ const sizes = {
   height: window.innerHeight
 }
 
-const params = {
-  data: 'test'
-}
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
 
 
-// Tweakpane for devolpment
-// localhost:5173/#   Open tweakpane 
-if(window.location.href.includes('#'))
-{
-  const pane = new Pane()
-  pane.addInput(params, 'data')
-}
+function init() {
 
-
-
-
-function init()
-{
-  
   scene = new THREE.Scene()
 
   renderer = new THREE.WebGLRenderer({
@@ -50,30 +34,28 @@ function init()
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 
-  camera = new THREE.PerspectiveCamera(45 , sizes.width / sizes.height , 0.1 , 3000 )
+  camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 3000)
   camera.position.z = 3
   scene.add(camera)
 
   control = new OrbitControls(camera, canvasWebGl)
   control.enableDamping = true
 
-  window.addEventListener('resize', () =>
-  { 
+  window.addEventListener('resize', () => {
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    renderer.setSize(sizes.width , sizes.height)
+    renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   })
 
 
 
-
-  box = new THREE.BoxGeometry(1,1,1)
-  material =new THREE.MeshBasicMaterial({color: 'red'})
+  box = new THREE.BoxGeometry(1, 1, 1)
+  material = new THREE.MeshBasicMaterial({ color: 'red' })
   mesh = new THREE.Mesh(box, material)
   scene.add(mesh)
 }
@@ -82,10 +64,9 @@ init()
 
 
 let delta;
-let fpsInterval, now, then , elapsed, startTime;
+let fpsInterval, now, then, elapsed, startTime;
 const clock = new THREE.Clock()
-function LockFrame(fps)
-{
+function LockFrame(fps) {
 
   fpsInterval = 1000 / fps
   then = window.performance.now()
@@ -96,14 +77,12 @@ LockFrame(60)
 
 
 
-function animate()
-{
+function animate() {
   requestAnimationFrame(animate)
-  now  = window.performance.now()
+  now = window.performance.now()
   elapsed = now - then
 
-  if(elapsed >= fpsInterval)
-  {
+  if (elapsed >= fpsInterval) {
     then = now - (elapsed % fpsInterval)
     const delta = clock.getDelta();
     // setInterval( () => 
@@ -119,16 +98,6 @@ function animate()
 
     stats.update()
     control.update()
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
   }
 }
-
-
-
-
-
-
-
-
-
-
